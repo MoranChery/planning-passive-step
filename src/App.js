a656a8abf06a5e06934c54a02c665e7fd10da061
chery <chery@post.bgu.ac.il>
2024-09-17 18:42:27 +0300

    Fixed css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <p> תכנון הצעד הפסיבי </p>
         <img src={logo} className="App-logo" alt="logo" />
